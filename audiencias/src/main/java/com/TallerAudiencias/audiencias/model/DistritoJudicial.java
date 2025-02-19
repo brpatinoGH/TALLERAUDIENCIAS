f9c88f407af4500f6e022d683523ce917a8513c3
Nao <nao.kakisu@gmail.com>
2025-02-19 19:11:38 -0300

    cambio en entidad DistritoJudicial

--- audiencias/src/main/java/com/TallerAudiencias/audiencias/model/DistritoJudicial.java
+++ audiencias/src/main/java/com/TallerAudiencias/audiencias/model/DistritoJudicial.java
@@ -3,11 +3,14 @@ package com.TallerAudiencias.audiencias.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "DistritoJudicial")
 public class DistritoJudicial {
     @Id
+    @Column(name = "usr_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, length = 150)
+    @Column(name = "dis_nombre")
     private String disNombre;
+
 }
