0bac5539a9a728972b89985c06912481548c68cd
brpatinoGH <145993802+brpatinoGH@users.noreply.github.com>
2025-02-19 17:21:25 -0300

    init commit

---
+++ audiencias/src/test/java/com/TallerAudiencias/audiencias/AudienciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TallerAudiencias.audiencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudienciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
