826ec8f6a5ad370c227f58259517535d58f58858
Nao <nao.kakisu@gmail.com>
2025-02-27 16:38:22 -0300

    Add of Repositories

--- audiencias/src/main/java/com/TallerAudiencias/audiencias/repositories/SalaRepository.java
+++ audiencias/src/main/java/com/TallerAudiencias/audiencias/repositories/SalaRepository.java
@@ -1,9 +1,9 @@
 package com.TallerAudiencias.audiencias.repositories;
 
-import com.TallerAudiencias.audiencias.entities.DistritoJudicial;
+import com.TallerAudiencias.audiencias.entities.Sala;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DistritoJudicialRepository extends JpaRepository<DistritoJudicial, Long> {
+public interface SalaRepository extends JpaRepository<Sala, Long> {
 }
--- audiencias/src/main/java/com/TallerAudiencias/audiencias/repositories/UsuarioRepository.java
+++ audiencias/src/main/java/com/TallerAudiencias/audiencias/repositories/UsuarioRepository.java
@@ -1,9 +1,9 @@
 package com.TallerAudiencias.audiencias.repositories;
 
-import com.TallerAudiencias.audiencias.entities.DistritoJudicial;
+import com.TallerAudiencias.audiencias.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DistritoJudicialRepository extends JpaRepository<DistritoJudicial, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
