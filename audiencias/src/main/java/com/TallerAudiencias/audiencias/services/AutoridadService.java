03d5a4f89bfaea0f924596f21f56777b252e49b0
Nao <nao.kakisu@gmail.com>
2025-03-06 14:46:46 -0300

    Services created

---
+++ audiencias/src/main/java/com/TallerAudiencias/audiencias/services/AutoridadService.java
@@ -0,0 +1,11 @@
+package com.TallerAudiencias.audiencias.services;
+
+import com.TallerAudiencias.audiencias.repositories.AutoridadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutoridadService {
+    @Autowired
+    private AutoridadRepository autoridadRepository;
+}
